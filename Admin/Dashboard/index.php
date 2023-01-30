705e964acb82efa966d48d3110eb79a6c28c24a2
Aditya Dubey <emailadityadubey@gmail.com>
2023-01-30 23:24:36 +0530

    check

--- Admin/Dashboard/index.php
+++ Admin/Dashboard/index.php
@@ -27,7 +27,6 @@
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="../assets/css/main.css">
   <link rel="stylesheet" href="../assets/css/dashboard.css">
-  <script src="../assets/js/dashboard.js"></script>
   <title>Police Darpan | Admin</title>
 </head>
 
@@ -392,6 +391,7 @@
     });
     
   </script>
+  <script src="../assets/js/dashboard.js"></script>
 
 </body>
 </html>
