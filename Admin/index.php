a3c0a4911734177b0f613acd879666b8576ffded
Aditya Dubey <emailadityadubey@gmail.com>
2023-02-05 18:48:42 +0530

    Update index.php
    For auto login.

--- Admin/index.php
+++ Admin/index.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if(isset($_SESSION['Edit_Access'])){
+ if($_SESSION['Edit_Access'] == "Allow"){
+   header("Location: ./Dashboard");
+   }
+}
 ?>
 <!doctype html>
 <html lang="en">
